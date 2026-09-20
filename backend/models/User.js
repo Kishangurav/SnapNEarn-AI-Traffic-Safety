3b2abde6f4c66c19451c85878d350597d669bb1c
Kishangurav <kishansgurav@gmail.com>
2026-09-20 18:11:45 +0530

    Fix admin login and password hashing

--- backend/models/User.js
+++ backend/models/User.js
@@ -109,7 +109,7 @@ userSchema.index({ preferredLocation: '2dsphere' });
 // Encrypt password before saving
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) {
-    next();
+    return next();
   }
   
   const salt = await bcrypt.genSalt(10);
